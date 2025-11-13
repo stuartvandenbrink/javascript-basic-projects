6695d6ea5f4c51f4512266289d845172b68c7076
Stuart VandenBrink <stuart.vandenbrink@sonarsource.com>
2025-11-13 15:03:16 -0600

    uncomment

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -4,7 +4,7 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
+  console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
